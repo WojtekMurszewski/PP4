210acbfc9dbae2737327a36921f234ac5b96e741
unknown <s222738@student.uek.krakow.pl>
2023-04-03 23:18:09 +0200

    03.04

---
+++ src/main/java/pl/s222738/productcatalog/ProductStorage.java
@@ -0,0 +1,13 @@
+package pl.s222738.productcatalog;
+
+import java.util.List;
+
+public interface ProductStorage {
+    List<Product> allProducts();
+
+    void add(Product newProduct);
+
+    Product loadById(String productId);
+
+    List<Product> allPublishedProducts();
+}
